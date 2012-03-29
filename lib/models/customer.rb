a94249bd84aec560a216bdc79c4f2c4119567f74
Jonan Scheffler <jonanscheffler@gmail.com>
2012-03-29 11:57:28 -0400

    Communicative variable names per reek suggestions.

--- lib/models/customer.rb
+++ lib/models/customer.rb
@@ -18,13 +18,13 @@ module SalesEngine
     end
 
     def transactions
-      @transactions ||= invoices.map { |i| i.transactions }
+      @transactions ||= invoices.map { |inv| inv.transactions }
     end
 
     def favorite_merchant
-      top_merchant_transaction = invoices.map do |i|
-        [i.merchant, i.transactions.count]
-      end.sort_by { |v| v[1] }.last
+      top_merchant_transaction = invoices.map do |inv|
+        [inv.merchant, inv.transactions.count]
+      end.sort_by { |value| value[1] }.last
 
       top_merchant_transaction.first
     end
